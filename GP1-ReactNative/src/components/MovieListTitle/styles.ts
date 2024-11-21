f416c1bab4310bf52c07b03d1b8b38093a8012be
anafortuna <alobofortuna@gmail.com>
2024-11-21 13:13:30 -0300

    Componente text

---
+++ GP1-ReactNative/src/components/MovieListTitle/styles.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+	titleText: {
+        color: '#fff',
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 12
+    },
+})
--- GP1-ReactNative/src/screens/Home/index.tsx
+++ GP1-ReactNative/src/screens/Home/index.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Text } from "react-native";
 import Body from "../../components/Body";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { RootStackParamList } from "../../Navigation/types";
+import { useNavigation } from "@react-navigation/native";
 
+type HomeScreenNavigationProp = StackNavigationProp<RootStackParamList, "Home">
 export const Home = () => {
+    const navigation = useNavigation<HomeScreenNavigationProp>();
+
     return (
         <Body customStyle={{ justifyContent: "center", padding: 16, alignItems: 'center' }}>
             <Text> Home </Text>
-
         </Body>
     )
 }
