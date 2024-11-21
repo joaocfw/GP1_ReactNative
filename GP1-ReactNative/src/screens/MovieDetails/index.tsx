76d142dec8738bc46a055173746e5008e5f8f1a1
anafortuna <alobofortuna@gmail.com>
2024-11-21 20:40:38 -0300

    Rota para MovieDetails

---
+++ GP1-ReactNative/src/screens/MovieDetails/index.tsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from "react";
+import { Image, Text, View, ScrollView } from "react-native";
+import Body from "../../components/Body"
+export const MovieDetails = () => {
+
+    return (
+
+        <Body customStyle={{}}>
+            <ScrollView>
+                <Text>Detail</Text>
+            </ScrollView>
+        </Body>
+    )
+}
+
+export default MovieDetails;
