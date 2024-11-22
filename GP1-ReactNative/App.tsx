c13f57098d3f90dcca7a6ffb9fae81c16965b60c
anafortuna <alobofortuna@gmail.com>
2024-11-21 22:27:27 -0300

    Ajuste App

--- GP1-ReactNative/App.tsx
+++ GP1-ReactNative/App.tsx
@@ -1,15 +1,6 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { MyStack } from "./src/Routes/MyStack";
-import { StatusBar } from "react-native";
+import { Routes } from "./src/Routes";
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <StatusBar backgroundColor='#161616' barStyle='light-content' />
-      <MyStack />
-      {/* <FavoritesProvider>
-        <Favorites/>
-      </FavoritesProvider> */}
-    </NavigationContainer>
-  );}
+  return  <Routes/>
+  }
