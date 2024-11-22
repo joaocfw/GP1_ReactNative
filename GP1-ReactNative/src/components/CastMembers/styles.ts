c4c85cdda6d84a8bb9e2a15b35fd81be52b2a026
anafortuna <alobofortuna@gmail.com>
2024-11-21 23:05:51 -0300

    MovieDetail

---
+++ GP1-ReactNative/src/components/CastMembers/styles.ts
@@ -0,0 +1,26 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    castListContainer: {
+        width: '100%',
+        paddingLeft: 10,
+        alignContent: 'center',
+        paddingVertical: 0,
+        marginBottom: 5,
+    },
+    castCard: {
+        marginRight: 14,
+        paddingVertical: 10,
+    },
+    castCardBorder: {
+        padding: 3,
+        borderWidth: 2,
+        borderColor: '#fff',
+        borderRadius: 50,
+    },
+    castCardImage: {
+        width: 64,
+        height: 64,
+        borderRadius: 50,
+    },
+})
