7fc2cc5961aeb41249e169e11f1885e142eeca9b
UserCardinot <lucas.cardinot@outlook.com>
2024-11-23 15:37:30 -0300

    pagina de favoritos, com opcao para adicionar e remover filme

--- GP1-ReactNative/App.tsx
+++ GP1-ReactNative/App.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { Routes } from "./src/Routes";
 import { AuthProvider } from "./src/Context/ContextSignIn";
+import { FavoritesProvider } from "./src/Context/FavoritesContext";
 
 export default function App() {
-  return <AuthProvider>
-  <Routes/>
-  </AuthProvider>
+  return (
+    <AuthProvider>
+      <FavoritesProvider>
+        <Routes />
+      </FavoritesProvider>
+    </AuthProvider>
+  );
 }
